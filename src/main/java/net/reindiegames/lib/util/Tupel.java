af91c1a1c0382f2a247d26370527059f6e4ac27a
ReIndieGames <reindiegames@gmail.com>
2017-05-04 22:09:00 +0200

    Validates Tupel.java + Adds Tupel-Empty functionality (partial/global)

--- src/main/java/net/reindiegames/lib/util/Tupel.java
+++ src/main/java/net/reindiegames/lib/util/Tupel.java
@@ -9,7 +9,7 @@ package net.reindiegames.lib.util;
  *         The generic Type of the second Value.
  *
  * @author Nils
- * @version Not Tested (Last Change: 04.05.2017)
+ * @version Valid (Last Change: 04.05.2017)
  * @since 04.05.2017
  */
 public class Tupel<F, S> {
@@ -63,6 +63,22 @@ public class Tupel<F, S> {
     }
 
     /**
+     * @param firstType
+     *         The generic Type of the first Value.
+     * @param secondType
+     *         The generic Type of the second Value.
+     * @param <F>
+     *         The generic Type of the first Value.
+     * @param <S>
+     *         The generic Type of the second Value.
+     *
+     * @return Returns an empty mutable Tupel defined by the two given Types.
+     */
+    public static <F, S> Tupel<F, S> genericEmpty(Class<F> firstType, Class<S> secondType) {
+        return Tupel.mutable(null, null);
+    }
+
+    /**
      * @return Retrieves the first Value of the Tupel.
      */
     public F first() {
@@ -81,6 +97,15 @@ public class Tupel<F, S> {
     public void setFirst(F first) {
         if (!mutable) throw new IllegalStateException("Cannot assign first Value! Tupel is immutable!");
         this.first = first;
+    }
+
+    /**
+     * Tests weather the first Value in this Tupel is equal to {@code null}.
+     *
+     * @return Returns TRUE if and only if the first Value in this Tupel is {@code null}.
+     */
+    public boolean isFirstEmpty() {
+        return first == null;
     }
 
     /**
@@ -105,11 +130,43 @@ public class Tupel<F, S> {
     }
 
     /**
+     * Tests weather the second Value in this Tupel is equal to {@code null}.
+     *
+     * @return Returns TRUE if and only if the second Value in this Tupel is {@code null}.
+     */
+    public boolean isSecondEmpty() {
+        return second == null;
+    }
+
+    /**
      * Tests weather this Tupel is mutable.
      *
      * @return Returns TRUE if and only if this Tupel is mutable.
      */
     public boolean isMutable() {
         return mutable;
+    }
+
+    /**
+     * Tests weather the two Values in this Tupel are equal to {@code null}.
+     *
+     * @return Returns TRUE if and only if the two Values in this Tupel are {@code null}.
+     */
+    public boolean isEmpty() {
+        return this.isFirstEmpty() && this.isSecondEmpty();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+
+        if (this.isMutable()) {
+            builder.append("Mutable");
+        } else {
+            builder.append("Immutable");
+        }
+
+        builder.append(" Tupel {First = ").append(first).append(", Second = ").append(second).append('}');
+        return builder.toString();
     }
 }
